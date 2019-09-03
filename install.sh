d21c441d35cfbeeae48d9e1da2e03ae9ddd366c7
byplayer <byplayer100@gmail.com>
2019-09-03 13:03:40 +0800

    :+1: Set install directory for actual tool
    Set actual install directory

--- install.sh
+++ install.sh
@@ -2,9 +2,13 @@
 
 set -ex
 
-virtualenv -p python3 python_tool
-source python_tool/bin/activate
-pip install jedi flake8 importmagic autopep8 yapf
+pushd `dirname ${0}`
+
+if [ ! -d python_tool ]; then
+  virtualenv -p python3 python_tool
+  source python_tool/bin/activate
+  pip install jedi flake8 importmagic autopep8 yapf
+fi
 
 if [ -d bin ]; then
   rm -r bin
@@ -17,3 +21,5 @@ app_list=(autopep8 flake8 pyflakes wheel yapf)
 for i in "${app_list[@]}"; do
   cp bin_templ bin/$i
 done
+
+popd
